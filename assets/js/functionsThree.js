c340ebe2542a323e4cbf9b55efac703ca746dba7
NightsHigh <soren913@gmail.com>
2025-03-12 14:31:02 +0100

    Fixed wrong console.log

--- assets/js/functionsThree.js
+++ assets/js/functionsThree.js
@@ -1,21 +1,16 @@
+// Her skal du skrive din anonyme funktion
+let myAnonFunc = function (message) {
+    console.log(message);
+};
+
+// Her kaldes, eller invokes din anonyme funktion gennem en variabel
+myAnonFunc('hellow World');
 
 
+// Her skal du skrive din arrow funktion
+let myArrowFunc = (message) => {
+    console.log(message);
+};
 
-// her skal du skrive din anonyme funktion
-let myAnonFunc=function helloWorld(message){
-    console.log(message)
-}
-
-
-// her kaldes, eller invokes din anonyme funktion gennem en variabel
-console.log(myAnonFunc('Hello World'));
-
-
-// her skal du skrive din arrow funktion
-let myArrowFunc=(message) => {
-    console.log(message)
-}
-
-
-// her kaldes, eller invokes din arrow funktion gennem en variabel
-console.log(myArrowFunc('Hello World'));
+// Her kaldes, eller invokes din arrow funktion gennem en variabel
+myArrowFunc('hellow World');
